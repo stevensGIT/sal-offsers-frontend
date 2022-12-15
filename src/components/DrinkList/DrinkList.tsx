70781d00abf4f17bdabbb61bc7f7c9a38774a025
Steven Boyle <steven.boyle@hedgehoglab.com>
2022-12-15 19:29:34 +0000

    speak to grambos api

--- src/components/DrinkList/DrinkList.tsx
+++ src/components/DrinkList/DrinkList.tsx
@@ -1,13 +1,24 @@
 import { DrinkCard } from "../DrinkCard";
 
+import { Heading, Flex } from "@chakra-ui/react";
+
 import { DrinkType } from "../types";
 
-export default function DrinkList({ drinks }: { drinks: DrinkType[] }) {
+export default function DrinkList({
+  name,
+  drinks,
+}: {
+  name: string;
+  drinks: DrinkType[];
+}) {
   return (
-    <>
+    <Flex direction="column">
+      <Heading color="white" mb="30px">
+        {name}
+      </Heading>
       {drinks.map((drink) => (
-        <DrinkCard key={drink.title} {...drink} />
+        <DrinkCard key={drink.id} {...drink} />
       ))}
-    </>
+    </Flex>
   );
 }
--- src/screen/Screen/Screen.tsx
+++ src/screen/Screen/Screen.tsx
@@ -2,15 +2,36 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 import { DrinkList } from "../../components/DrinkList";
 
-import { drinksList } from "../../drinks";
+import { useQuery } from "react-query";
+
+const ENDPOINT = "https://4151-51-148-179-254.eu.ngrok.io/api/drinks";
 
 export default function Screen() {
-  const { drinks } = drinksList;
+  const { data } = useQuery(["drinks"], async () => {
+    const res = await fetch(ENDPOINT);
+    const data = await res.json();
+
+    const categories = Object.keys(data);
+
+    const drinks = categories.map((category) => ({
+      name: category,
+      drinks: data[category].map((drink: any) => ({
+        id: drink.id,
+        title: drink.brewery_name,
+        notes: drink.description,
+        logo: drink.image_url,
+        abv: drink.abv,
+      })),
+    }));
+
+    return drinks;
+  });
 
   return (
     <SimpleGrid columns={2} spacing="40px" p="40px" minChildWidth="500px">
-      <DrinkList drinks={drinks} />
-      <DrinkList drinks={drinks} />
+      {data?.map((drink) => (
+        <DrinkList key={drink.name} name={drink.name} drinks={drink.drinks} />
+      ))}
     </SimpleGrid>
   );
 }
